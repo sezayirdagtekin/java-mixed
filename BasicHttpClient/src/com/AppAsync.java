10ecf30930642afe4ece637ece60b04230754766
Sezayir Dağtekin <sezayir@hotmail.com>
2021-05-15 08:49:31 +0300

    Async http client

---
+++ BasicHttpClient/src/com/AppAsync.java
@@ -0,0 +1,43 @@
+package com;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class AppAsync {
+
+	private static HttpClient client;
+
+	public static void main(String[] args) throws IOException {
+
+		client = HttpClient.newHttpClient();
+
+		var futures=Files.lines(Path.of("url.txt"))
+				.map(AppAsync::validateLink).collect(Collectors.toList());
+		
+		futures.stream().map(CompletableFuture::join).forEach(System.out::println);
+
+	}
+
+	private static CompletableFuture<String> validateLink(String link) {
+
+		HttpRequest request = HttpRequest.newBuilder(URI.create(link)).GET().build();
+
+		return client.sendAsync(request, HttpResponse.BodyHandlers.discarding()).thenApply(AppAsync::responseTostring)
+				.exceptionally(e -> String.format("%s-> %s", link, link, false));
+
+	}
+
+	private static String responseTostring(HttpResponse<Void> response) {
+		int status = response.statusCode();
+		boolean success = status > 200 && status <= 299;
+		return String.format("%s-> %s(status:%s)", response.uri(), success, status);
+
+	}
+}
